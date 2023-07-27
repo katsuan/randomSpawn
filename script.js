a10ffcbfc86f34f79030aaf8e4526e55588de4a6
katsushima <>
2023-07-27 12:16:45 +0900

    [add]スペースを選択できるようにカスタムパラメーターを作成。

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 // パラメーターを定義する
 function getParameters() {
     const params = new URL(window.location.href).searchParams;
-    const minX = params.get("mX") || "10";
-    const maxX = params.get("MX") || "1400";
-    const minY = params.get("mY") || "10";
-    const maxY = params.get("MY") || "1900";
-    console.log(minX);
+    const defaultValue = (param, value) => params.get(param) || value.toString();
+    const minX = defaultValue("x", 10);
+    const maxX = defaultValue("X", 1400);
+    const minY = defaultValue("y", 10);
+    const maxY = defaultValue("Y", 1900);
+    const redirectTo = defaultValue("space", "wao-mirai");
+
     return {
         minX: parseInt(minX, 10),
         maxX: parseInt(maxX, 10),
         minY: parseInt(minY, 10),
         maxY: parseInt(maxY, 10),
+        redirectTo,
     };
 }
 
 // 範囲内のランダムな座標を生成する関数
 function generateRandomCoordinates() {
-
     const parameters = getParameters();
-    var minX = parameters.minX;  // 範囲の最小X座標
-    var maxX = parameters.maxX; // 範囲の最大X座標
-    var minY = parameters.minY; // 範囲の最小Y座標
-    var maxY = parameters.maxY; // 範囲の最大Y座標
+    const { minX, maxX, minY, maxY } = parameters;
 
-    var randomX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
-    var randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+    const randomX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+    const randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
 
     return randomX + ',' + randomY;
 }
 
 // ランダムな座標を含むURLにリダイレクトする
-window.location.href = 'https://wao-mirai.ovice.in/@' + generateRandomCoordinates();
+const redirectTo = getParameters().redirectTo;
+const redirectURL = 'https://' + redirectTo + '.ovice.in/@' + generateRandomCoordinates()
+window.location.href = redirectURL;
